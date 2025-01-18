b61392260f00639446764246c9291e30ffa6095f
Astro <rgrxmahabub.official@gmail.com>
2025-01-18 23:38:52 +0600

    updated

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <section className="flex flex-col items-center justify-center h-screen">
-      <NotFoundAnimation />
       <Link href="/">
         <button className="bg-primary hover:bg-transparent duration-300 hover:text-primary text-white w-full font-bold px-20 py-3 rounded-full hover-fade border-2 border-primary mt-10">
           Return Home
